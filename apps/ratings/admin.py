0fa5aa37b5122a8b4fd9d8261e7ab06a064ce248
Tonia Roganti <toniaroganti@gmail.com>
2022-04-05 09:26:45 -0400

    added rating model and serializer

--- apps/ratings/admin.py
+++ apps/ratings/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Rating
+
+
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ["rater", "agent", "rating"]
+
+admin.site.register(Rating, RatingAdmin)
+
 # Register your models here.
